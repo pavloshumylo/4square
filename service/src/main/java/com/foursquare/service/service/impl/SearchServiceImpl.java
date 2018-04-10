b9cf94bc0d0365213c4752e0d7cd93e92a033200
pavlo.shumylo <pavlo.shumylo@gmail.com>
2018-04-10 12:24:00 +0300

    Add minor changes to SearchServiceImpl

--- service/src/main/java/com/foursquare/service/service/impl/SearchServiceImpl.java
+++ service/src/main/java/com/foursquare/service/service/impl/SearchServiceImpl.java
@@ -1,18 +1,15 @@
 package com.foursquare.service.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.foursquare.dao.SearchDao;
 import com.foursquare.dto.SearchResponseDto;
 import com.foursquare.dto.VenueDto;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.foursquare.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.*;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -30,28 +27,23 @@ public class SearchServiceImpl implements SearchService {
     }
 
     private void mapFromJson(String json, SearchResponseDto searchResponse) throws IOException {
-        JsonNode venueDtoNode = new ObjectMapper().readTree(json);
-        JsonNode jsonNode = venueDtoNode.get("response").get("venues");
-        Iterator <JsonNode> elements = jsonNode.elements();
-        Iterable<JsonNode> iterable = () -> elements;
-
-        Stream<JsonNode> stream  = StreamSupport.stream(iterable.spliterator(), false);
-        stream.forEach((node) -> {
+        JsonNode jsonNode = new ObjectMapper().readTree(json);
+        JsonNode venuesNode = jsonNode.get("response").get("venues");
+        venuesNode.forEach((venueNode) -> {
             VenueDto venueDto = new VenueDto();
-            JsonNode tempNode = node;
-            venueDto.setId(tempNode.get("id").textValue());
-            venueDto.setName(tempNode.get("name").textValue());
+            venueDto.setId(venueNode.get("id").textValue());
+            venueDto.setName(venueNode.get("name").textValue());
 
-            JsonNode phone = tempNode.get("contact").get("phone");
-            if (phone != null) {
-                venueDto.setPhone(phone.textValue());
+            JsonNode phoneNode = venueNode.get("contact").get("phone");
+            if (phoneNode != null) {
+                venueDto.setPhone(phoneNode.textValue());
             } else {
                 venueDto.setPhone("No phone available");
             }
 
-            JsonNode address = tempNode.get("location").get("address");
-            if (address != null) {
-                venueDto.setAddress(address.textValue());
+            JsonNode addressNode = venueNode.get("location").get("address");
+            if (venueNode.get("location").get("address") != null) {
+                venueDto.setAddress(addressNode.textValue());
             } else {
                 venueDto.setAddress("No address available");
             }
