284b8620cfef5a6631b7eed6722773e85e5cafc7
Pavlo <pavlo.shumylo@gmail.com>
2018-04-14 23:57:43 +0300

    Add usage of 'MockSearchDaoImpl' in test layer instead of 'SearchDaoImpl'

--- service/src/test/java/com/foursquare/integration/SearchServiceIntegrationTest.java
+++ service/src/test/java/com/foursquare/integration/SearchServiceIntegrationTest.java
@@ -1,19 +1,22 @@
 package com.foursquare.integration;
 
+import com.foursquare.dao.SearchDao;
+import com.foursquare.dao.dao.impl.MockSearchDaoImpl;
 import com.foursquare.dto.SearchResponseDto;
 import com.foursquare.dto.VenueDto;
 import com.foursquare.service.SearchService;
+import com.foursquare.service.service.impl.SearchServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class SearchServiceIntegrationTest {
 
     @Autowired
@@ -74,5 +77,19 @@ public class SearchServiceIntegrationTest {
 
         assertEquals(searchResponseDtoExpected.getVenues().get(0).getPhone(),
                 searchResponseDtoActual.getVenues().get(0).getPhone());
+    }
+
+    @TestConfiguration
+    static class ContextConfiguration {
+
+        @Bean
+        public SearchService searchService() {
+            return new SearchServiceImpl();
+        }
+
+        @Bean
+        public SearchDao searchDao(){
+            return new MockSearchDaoImpl();
+        }
     }
 }
