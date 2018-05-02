cde0c697b6b8d8e21aaf13c26cae29f1db6aec33
Pavlo <pavlo.shumylo@gmail.com>
2018-05-03 00:01:15 +0300

    Add unit test for LoggingAspect

--- service/src/test/java/com/foursquare/logging/LoggingAspectTest.java
+++ service/src/test/java/com/foursquare/logging/LoggingAspectTest.java
@@ -1,49 +1,46 @@
 package com.foursquare.logging;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoggingAspectTest {
 
-    private static class LoggingAspectNested {
-
-        @LoggingInvocation(logLevel = LoggingLevel.INFO)
-        public void invocationOfAspect() {
-        }
-    }
-
-    @SpringBootApplication
-    public static class ConfigNested {
-
-        @Bean
-        public LoggingAspectNested loggingAspectNested() {
-            return new LoggingAspectNested();
-        }
-    }
-
-    @MockBean
+    @Autowired
     private LoggingAspect loggingAspect;
 
-    @Autowired
-    private LoggingAspectNested loggingAspectNested;
+    @Mock
+    private JoinPoint joinPoint;
+    @Mock
+    private LoggingInvocation loggingInvocation;
+    @Mock
+    private Signature signature;
+    @Mock
+    private Object object;
 
     @Test
-    public void testLoggingAspect() {
-        loggingAspectNested.invocationOfAspect();
-        verify(loggingAspect, times(1)).methodInvocationLogging(any(JoinPoint.class),
-                any(LoggingInvocation.class));
+    public void testMethodInvocationLogging_ShouldInvokedOneTime() {
+        MockitoAnnotations.initMocks(this);
+        LoggingLevel level = spy(LoggingLevel.INFO);
+
+        when(loggingInvocation.logLevel()).thenReturn(level);
+        doNothing().when(level).executeLogging(anyString(), anyString(), any(Object[].class));
+        when(joinPoint.getTarget()).thenReturn(object);
+        when(joinPoint.getSignature()).thenReturn(signature);
+        when(signature.getName()).thenReturn("Name");
+        when(joinPoint.getArgs()).thenReturn(new Object[4]);
+
+        loggingAspect.methodInvocationLogging(joinPoint, loggingInvocation);
+        verify(level, times(1)).executeLogging(anyString(), anyString(), any(Object[].class));
     }
 }
