28ea58ce87259f0352a309ae2b6dbfc53e62afbf
pavlo.shumylo <pavlo.shumylo@gmail.com>
2018-05-03 15:02:45 +0300

    Add minor changes to 'LoggingLeve'

--- service/src/test/java/com/foursquare/logging/LoggingLevelTest.java
+++ service/src/test/java/com/foursquare/logging/LoggingLevelTest.java
@@ -17,6 +17,8 @@ import static org.mockito.Mockito.*;
 @PrepareForTest(LoggerFactory.class)
 public class LoggingLevelTest {
 
+    private static final String LOGGER_MESSAGE =
+            "Method invocation. Class: firstClass. Method: secondMethod. Params: [first, second]";
     @Mock
     private Logger logger;
 
@@ -31,39 +33,39 @@ public class LoggingLevelTest {
     public void testExecuteLogging_ShouldInvokeTraceLogger() {
         doNothing().when(logger).trace(anyString());
 
-        LoggingLevel.TRACE.executeLogging("firstClass", "secondMethod", new Object[2]);
-        verify(logger).trace("Method invocation. Class: firstClass. Method: secondMethod. Params: [null, null]");
+        LoggingLevel.TRACE.executeLogging("firstClass", "secondMethod", new Object[]{"first", "second"});
+        verify(logger).trace(LOGGER_MESSAGE);
     }
 
     @Test
     public void testExecuteLogging_ShouldInvokeDebugLogger() {
         doNothing().when(logger).debug(anyString());
 
-        LoggingLevel.DEBUG.executeLogging("firstClass", "secondMethod", new Object[2]);
-        verify(logger).debug("Method invocation. Class: firstClass. Method: secondMethod. Params: [null, null]");
+        LoggingLevel.DEBUG.executeLogging("firstClass", "secondMethod", new Object[]{"first", "second"});
+        verify(logger).debug(LOGGER_MESSAGE);
     }
 
     @Test
     public void testExecuteLogging_ShouldInvokeInfoLogger() {
         doNothing().when(logger).info(anyString());
 
-        LoggingLevel.INFO.executeLogging("firstClass", "secondMethod", new Object[2]);
-        verify(logger).info("Method invocation. Class: firstClass. Method: secondMethod. Params: [null, null]");
+        LoggingLevel.INFO.executeLogging("firstClass", "secondMethod", new Object[]{"first", "second"});
+        verify(logger).info(LOGGER_MESSAGE);
     }
 
     @Test
     public void testExecuteLogging_ShouldInvokeWarnLogger() {
         doNothing().when(logger).warn(anyString());
 
-        LoggingLevel.WARN.executeLogging("firstClass", "secondMethod", new Object[2]);
-        verify(logger).warn("Method invocation. Class: firstClass. Method: secondMethod. Params: [null, null]");
+        LoggingLevel.WARN.executeLogging("firstClass", "secondMethod", new Object[]{"first", "second"});
+        verify(logger).warn(LOGGER_MESSAGE);
     }
 
     @Test
     public void testExecuteLogging_ShouldInvokeErrorLogger() {
         doNothing().when(logger).error(anyString());
 
-        LoggingLevel.ERROR.executeLogging("firstClass", "secondMethod", new Object[2]);
-        verify(logger).error("Method invocation. Class: firstClass. Method: secondMethod. Params: [null, null]");
+        LoggingLevel.ERROR.executeLogging("firstClass", "secondMethod", new Object[]{"first", "second"});
+        verify(logger).error(LOGGER_MESSAGE);
     }
 }
