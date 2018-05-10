2431635abff6da65f04efc27cb03a5558da4759c
pavlo.shumylo <pavlo.shumylo@gmail.com>
2018-05-10 13:39:50 +0300

    Add smoke test using H2 db

---
+++ service/src/test/java/com/foursquare/repository/UserRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.foursquare.repository;
+
+import com.foursquare.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User firstUserExpected, secondUserExpected;
+
+    @Before
+    public void init() {
+        firstUserExpected = new User();
+        secondUserExpected = new User();
+    }
+
+    @Test
+    public void testSave_ShouldSaveAndReturnExpectedUser() {
+        User firstUserActual = userRepository.save(firstUserExpected);
+        User secondUserActual = userRepository.save(secondUserExpected);
+        long countActual = userRepository.count();
+
+        assertEquals(firstUserExpected, firstUserActual);
+        assertEquals(secondUserExpected, secondUserActual);
+        assertEquals(2, countActual);
+    }
+
+    @Test
+    public void testGet_ShouldReturnProperUserById() {
+        entityManager.persist(firstUserExpected);
+        entityManager.persist(secondUserExpected);
+
+        User firstUserActual = userRepository.getOne(1);
+        User secondUserActual = userRepository.getOne(2);
+
+        assertEquals(firstUserExpected, firstUserActual);
+        assertEquals(secondUserExpected, secondUserActual);
+    }
+
+    @Test
+    public void testDelete_ShouldDeleteProperUser() {
+        entityManager.persist(firstUserExpected);
+        entityManager.persist(secondUserExpected);
+
+        userRepository.delete(firstUserExpected);
+        userRepository.delete(secondUserExpected);
+        assertThat(userRepository.findAll()).isEmpty();
+    }
+}
