da4febe9e8bbcc9fb925bfa8bec1b45197a9e3c6
pavlo.shumylo <pavlo.shumylo@gmail.com>
2018-05-14 18:09:52 +0300

    Added 'OAuth2' security feature

--- service/src/main/java/com/foursquare/controller/SearchController.java
+++ service/src/main/java/com/foursquare/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.foursquare.controller;
 import com.foursquare.dto.SearchResponseDto;
 import com.foursquare.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +13,7 @@ public class SearchController {
     private SearchService searchService;
 
     @GetMapping(value = "/search")
+    @PreAuthorize(value = "hasRole('ADMIN')")
     public SearchResponseDto search(@RequestParam(value = "near") String near, @RequestParam(value = "query") String query, @RequestParam(value = "limit") String limit) {
         return searchService.search(near, query, limit);
     }
--- service/src/main/java/com/foursquare/repository/UserRepository.java
+++ service/src/main/java/com/foursquare/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.foursquare.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByName(String name);
 }
