4e84c93d2f8255367204c5afb9691995aa8ccb8a
Pavlo <pavlo.shumylo@gmail.com>
2018-05-17 23:22:21 +0300

    Add 'User' role

--- service/src/main/java/com/foursquare/controller/SearchController.java
+++ service/src/main/java/com/foursquare/controller/SearchController.java
@@ -13,7 +13,7 @@ public class SearchController {
     private SearchService searchService;
 
     @GetMapping(value = "/search")
-    @PreAuthorize(value = "hasRole('ADMIN')")
+    @PreAuthorize(value = "hasAnyRole('ADMIN', 'USER')")
     public SearchResponseDto search(@RequestParam(value = "near") String near, @RequestParam(value = "query") String query, @RequestParam(value = "limit") String limit) {
         return searchService.search(near, query, limit);
     }
